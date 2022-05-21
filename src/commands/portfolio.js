d1b208a34f4ba3682d68c8b54f4aa4d7dfb92aa5
Mezo <dominik@koch-bautechnik.de>
2022-05-21 22:18:33 +0200

    Bug Fix
    Co-authored-by: Finn Kötting <FallenBlackDev@users.noreply.github.com>

--- src/commands/portfolio.js
+++ src/commands/portfolio.js
@@ -18,65 +18,103 @@ module.exports = {
     if (interaction.options.getUser("user")) {
      await userschema.findOne({ userId: interaction.options.getUser("user").id }).then(result => {
         if (!result) {
-          const errorembed1 = new MessageEmbed()
-          .setColor("RED")
-          .setTitle("Wopps")
-          .setDescription("This user doesn't have a portfolio yet.")
-          .setTimestamp();
-
-         interaction.reply({ embeds: [errorembed1] });
-        }
-      });
-
-      } else if (interaction.options.getUser("user")) {
-       await userschema.findOne({ userId: interaction.user.id }).then(result => {
-          if (!result) {
+          if(interaction.options.getUser("user").id == interaction.user.id) {
             const errorembed = new MessageEmbed()
             .setColor("RED")
             .setTitle("Wopps")
             .setDescription("You dont seem to have a portfolio yet.")
             .setTimestamp();
   
-            interaction.reply({ embeds: [errorembed] });
+            return interaction.reply({ embeds: [errorembed] });
+          } else {
+            const errorembed1 = new MessageEmbed()
+            .setColor("RED")
+            .setTitle("Wopps")
+            .setDescription("This user doesn't have a portfolio yet.")
+            .setTimestamp();
+  
+           return interaction.reply({ embeds: [errorembed1] });
           }
-        });
-
-  } else {
-
-    const portfolioembed = new MessageEmbed()
-
-      .setColor("#5865f4")
-      .setTitle(` ${interaction.user.username} portfolio`)
-      .setDescription("This is your Portfolio")
-      .setTimestamp();
-
-      const components = new MessageActionRow()
-      .setComponents(
-        new MessageButton()
-        .setCustomId('977331897866809344-mainmenu')
-        .setLabel('🏠')
-        .setStyle('PRIMARY'),
-        new MessageButton()
-        .setCustomId('977331897866809344-previouspage')
-        .setLabel('←')
-        .setStyle('PRIMARY'),
-        new MessageButton()
-        .setCustomId('977331897866809344-nextpage')
-        .setLabel('→')
-        .setStyle('PRIMARY'),
-        new MessageButton()
-        .setCustomId(`977331897866809344-edit`)
-        .setLabel('🔧')
-        .setStyle('PRIMARY')
-
-
-        )
-
-    await interaction.reply({
-      embeds: [portfolioembed],
-      components: [components]
-    }
-    );
+        } else {
+          const portfolioembed = new MessageEmbed()
+        
+          .setColor("#5865f4")
+          .setTitle(` ${interaction.user.username} portfolio`)
+          .setDescription("This is the Portfolio of <@" + interaction.options.getUser("user").id + ">")
+          .setTimestamp();
+        
+          const components = new MessageActionRow()
+          .setComponents(
+            new MessageButton()
+            .setCustomId('977331897866809344-mainmenu')
+            .setLabel('🏠')
+            .setStyle('PRIMARY'),
+            new MessageButton()
+            .setCustomId('977331897866809344-previouspage')
+            .setLabel('←')
+            .setStyle('PRIMARY'),
+            new MessageButton()
+            .setCustomId('977331897866809344-nextpage')
+            .setLabel('→')
+            .setStyle('PRIMARY'),
+            new MessageButton()
+            .setCustomId(`977331897866809344-edit`)
+            .setLabel('🔧')
+            .setStyle('PRIMARY')
+            )
+          
+        interaction.reply({
+          embeds: [portfolioembed],
+          components: [components]
+        }
+        );
+        }
+      });
+      } else {
+        await userschema.findOne({ userId: interaction.user.id }).then(result => {
+          if(!result) {
+            const errorembed = new MessageEmbed()
+            .setColor("RED")
+            .setTitle("Wopps")
+            .setDescription("You dont seem to have a portfolio yet.")
+            .setTimestamp();
+  
+            return interaction.reply({ embeds: [errorembed] });
+          } else {
+            const portfolioembed = new MessageEmbed()
+        
+            .setColor("#5865f4")
+            .setTitle(` ${interaction.user.username} portfolio`)
+            .setDescription("This is your Portfolio")
+            .setTimestamp();
+          
+            const components = new MessageActionRow()
+            .setComponents(
+              new MessageButton()
+              .setCustomId('977331897866809344-mainmenu')
+              .setLabel('🏠')
+              .setStyle('PRIMARY'),
+              new MessageButton()
+              .setCustomId('977331897866809344-previouspage')
+              .setLabel('←')
+              .setStyle('PRIMARY'),
+              new MessageButton()
+              .setCustomId('977331897866809344-nextpage')
+              .setLabel('→')
+              .setStyle('PRIMARY'),
+              new MessageButton()
+              .setCustomId(`977331897866809344-edit`)
+              .setLabel('🔧')
+              .setStyle('PRIMARY')
+              )
+            
+          interaction.reply({
+            embeds: [portfolioembed],
+            components: [components]
+          }
+          );
+          }
+      })
     }
   },
 };
