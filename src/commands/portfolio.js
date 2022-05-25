bd1c1fb1545844b2dcd3fd63df6b9b744ae3c59c
Dominik Koch <dominik@koch-bautechnik.de>
2022-05-25 23:04:24 +0000

    Changed by Black#0001

--- src/commands/portfolio.js
+++ src/commands/portfolio.js
@@ -13,38 +13,36 @@ module.exports = {
         .setDescription("The user which description you want to see")
         .setRequired(false)
     ),
-  async execute(interaction, client) {
+  async execute(interaction) {
+    if (interaction.options.getUser("user")) {
       await userschema
-        .findOne({ userId: interaction.options.getUser("user")?.id || interaction.user.id })
+        .findOne({ userId: interaction.options.getUser("user").id })
         .then(async (result) => {
           if (!result) {
-            let errorembed = new MessageEmbed()
-            .setColor("RED")
-            .setTitle("Wopps");
+            if (interaction.options.getUser("user").id == interaction.user.id) {
+              const errorembed = new MessageEmbed()
+                .setColor("RED")
+                .setTitle("Wopps")
+                .setDescription(
+                  "You dont seem to have a portfolio yet. You can create one using **/register**"
+                );
 
-            
+              return await interaction.reply({
+                embeds: [errorembed],
+                ephemeral: true,
+              });
+            } else {
+              const errorembed = new MessageEmbed()
+                .setColor("RED")
+                .setTitle("Wopps")
+                .setDescription("This user doesn't have a portfolio yet.");
 
-            // if(interaction.options.getUser("user")) {
-                if (interaction.options.getUser("user")?.id == interaction.user.id) {
-                errorembed.setDescription("You dont seem to have a portfolio yet. You can create one using **/register**");
-
-                return await interaction.reply({
-                  embeds: [errorembed],
-                  ephemeral: true,
-                });
-              } else {
-                errorembed.setDescription("This user doesn't have a portfolio yet.");
-
-                return await interaction.reply({
-                  embeds: [errorembed],
-                  ephemeral: true,
-                });
-              }
-            // }
-            
+              return await interaction.reply({
+                embeds: [errorembed],
+                ephemeral: true,
+              });
+            }
           } else {
-            let user = client.users.cache.get(interaction.options.getUser("user")?.id || interaction.user.id);
-
             let badges = "`None`";
             let verified = "";
             if (result.badges.length !== 0) badges = "";
@@ -63,10 +61,10 @@ module.exports = {
               .setColor(`${result.embedcolor}`)
               .setTitle(
                 `${verified} ${
-                  user.username
+                  interaction.options.getUser("user").username
                 }'s portfolio`
               )
-              .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+              .setThumbnail(interaction.options.getUser("user").avatarURL())
               .setDescription(`> ${result.description}`)
               .addFields(
                 {
@@ -86,7 +84,7 @@ module.exports = {
                 }
               );
 
-            let components = new MessageActionRow().setComponents(
+            const components = new MessageActionRow().setComponents(
               new MessageButton()
                 .setCustomId("mainmenu")
                 .setLabel("🏠")
@@ -105,85 +103,18 @@ module.exports = {
                 .setCustomId("quicklinks")
                 .setLabel("Quicklinks")
                 .setStyle("PRIMARY")
-                .setEmoji("🔗"),
+                .setEmoji("🔗")
             );
 
-            setTimeout(async function () {
-              components.components[0].setDisabled(true);
-              components.components[1].setDisabled(true);
-              components.components[2].setDisabled(true);
-              components.components[3].setDisabled(true);
-              components.components[4].setDisabled(true);
-    
-             await interaction.editReply({embeds: [portfolioembed], components: [components]})
-        
-            }, 120000);
-
-            if(user.id == interaction.user.id) {
-              components.addComponents(
-                new MessageButton()
-                .setCustomId(`like`)
-                .setLabel("🤍")
-                .setStyle("DANGER")
-                .setDisabled(true),
-              )
-            } else {
-              components.addComponents(
-                new MessageButton()
-                .setCustomId(`like`)
-                .setLabel("🤍")
-                .setStyle("DANGER")
-              ) 
-            }
-            
-            let sentMessage = await interaction.reply({
+            await interaction.reply({
               embeds: [portfolioembed],
               components: [components],
             });
 
-            const listener = interaction.channel.createMessageComponentCollector();
-
+            const listener =
+              interaction.channel.createMessageComponentCollector();
             listener.on("collect", async (buttonInteraction) => {
               switch (buttonInteraction.customId) {
-                case "like":
-                  if (result.likes.includes(buttonInteraction.user.id)) {
-                    result.likes.splice(
-                      result.likes.findIndex(
-                        (l) => l == buttonInteraction.user.id
-                      ),
-                      1
-                    );
-                    await result.save();
-
-                    const likeEmbed = new MessageEmbed()
-                      .setColor("#5865f4")
-                      .setTitle("Like")
-                      .setDescription(
-                        "You removed your like from this portfolio."
-                      );
-
-                    return await buttonInteraction.reply({
-                      embeds: [likeEmbed],
-                      ephemeral: true,
-                    });
-                  } else {
-                    result.likes.push(buttonInteraction.user.id);
-
-                    await result.save();
-
-                    const likeEmbed = new MessageEmbed()
-                      .setColor("#5865f4")
-                      .setTitle("Like")
-                      .setDescription("You liked this portfolio.");
-
-                    await buttonInteraction.reply({
-                      embeds: [likeEmbed],
-                      ephemeral: true,
-                      fetchReply: true
-                    });
-                  }
-                  break;
-
                 default:
                   const errorembed = new MessageEmbed()
                     .setColor("RED")
@@ -192,17 +123,94 @@ module.exports = {
                       "This button does not seem to work properly."
                     );
 
-                    await buttonInteraction.reply({
+                  await buttonInteraction.reply({
                     embeds: [errorembed],
                     ephemeral: true,
                   });
-                  
                   break;
               }
             });
           }
         });
+    } else {
+      await userschema
+        .findOne({ userId: interaction.user.id })
+        .then(async (result) => {
+          if (!result) {
+            const errorembed = new MessageEmbed()
+              .setColor("RED")
+              .setTitle("Wopps")
+              .setDescription(
+                "You dont seem to have a portfolio yet. You can create one using **/register**"
+              );
 
-        
+            return interaction.reply({ embeds: [errorembed], ephemeral: true });
+          } else {
+            let badges = "`None`";
+            let verified = "";
+            if (result.badges.length !== 0) badges = "";
+            if (result.badges.includes("staff"))
+              badges += "<:Staff:977994687312969738> ";
+            if (result.badges.includes("developer"))
+              badges += "<:Developer:977996164458766396> ";
+            if (result.badges.includes("verified"))
+              verified = "<:Verified:977994824038875166>";
+            if (result.badges.includes("partner"))
+              badges += "<:Partner:977994687208116274> ";
+            if (result.badges.includes("featured"))
+              badges += "<:Featured:977994686851579906> ";
+
+            const portfolioembed = new MessageEmbed()
+              .setColor(`${result.embedcolor}`)
+              .setTitle(`${verified} ${interaction.user.username}'s profile`)
+              .setThumbnail(interaction.user.avatarURL())
+              .setDescription(`> ${result.description}`)
+              .addFields(
+                {
+                  name: "User Badges:",
+                  value: `${badges}`,
+                  inline: false,
+                },
+                {
+                  name: "Likes",
+                  value: `❤️ ${result.likes.length}`,
+                  inline: false,
+                },
+                {
+                  name: "Portfolio created:",
+                  value: `<t:${result.userSince}:F>`,
+                  inline: false,
+                }
+              );
+
+            const components = new MessageActionRow().setComponents(
+              new MessageButton()
+                .setCustomId("mainmenu")
+                .setLabel("🏠")
+                .setStyle("SUCCESS"),
+              new MessageButton()
+                .setCustomId("projects")
+                .setLabel("Projects")
+                .setStyle("PRIMARY")
+                .setEmoji("📝"),
+              new MessageButton()
+                .setCustomId("occupation")
+                .setLabel("Occupation")
+                .setStyle("PRIMARY")
+                .setEmoji("💼"),
+              new MessageButton()
+                .setCustomId("quicklinks")
+                .setLabel("Quicklinks")
+                .setStyle("PRIMARY")
+                .setEmoji("🔗")
+            );
+
+            await interaction.reply({
+              embeds: [portfolioembed],
+              components: [components],
+            });
+          }
+        });
+    }
   },
 };
