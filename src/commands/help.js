bc3afb174b08cf587ef346bb33a9543a2fc9c1de
Mezo <dominik@koch-bautechnik.de>
2022-05-28 01:10:19 +0200

    Help command update [Unfinished]

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,10 +4,34 @@ const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("help")
-    .setDescription("Returns the bot's help embed"),
+    .setDescription("Returns the bot's help embed")
+    .addSubcommand((subCommand) =>
+      subCommand
+        .setName("botowner")
+        .setDescription("Shows the hashrate of current nvidia graphics cards")
+    )
+    .addSubcommand((subCommand) =>
+      subCommand
+        .setName("botowner")
+        .setDescription("Shows the hashrate of current nvidia graphics cards")
+    )
+    .addSubcommand((subCommand) =>
+      subCommand
+        .setName("utility")
+        .setDescription("Shows the hashrate of current amd graphics cards")
+    ),
 
   async execute(interaction, client) {
-    const embed = new MessageEmbed()
+    const embed = new MessageEmbed();
 
-  }
-}
+    await interaction.reply({
+      embeds: [embed],
+      components: [button],
+    });
+    setTimeout(function () {
+      button.components[0].setDisabled(true);
+      button.components[1].setDisabled(true);
+      interaction.editReply({ embeds: [embed], components: [button] });
+    }, 120000);
+  },
+};
