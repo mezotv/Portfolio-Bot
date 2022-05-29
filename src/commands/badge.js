6475983d88b7944f520e6b427e126822cad01bf2
Mezo <dominik@koch-bautechnik.de>
2022-05-29 20:33:20 +0200

    badges system fix

--- src/commands/badge.js
+++ src/commands/badge.js
@@ -1,5 +1,10 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageEmbed, MessageActionRow, MessageButton, MessageSelectMenu } = require("discord.js");
+const {
+  MessageEmbed,
+  MessageActionRow,
+  MessageButton,
+  MessageSelectMenu,
+} = require("discord.js");
 
 const { Staff } = require("../../config.json");
 const userschema = require("../util/Schemas/userSchema");
@@ -8,50 +13,49 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("badge")
     .setDescription("Manage badges of users")
-    .addUserOption(user =>
+    .addUserOption((user) =>
       user
         .setRequired(true)
         .setDescription("The user to manage badges for")
         .setName("user")
     )
-    .addStringOption(option =>
-      option.setName('category')
-        .setDescription('The gif category')
+    .addStringOption((option) =>
+      option
+        .setName("category")
+        .setDescription("The gif category")
         .setRequired(true)
-        .addChoices({
-          name: 'add',
-          value: 'add'
-        },
+        .addChoices(
           {
-            name: 'remove',
-            value: 'remove'
+            name: "add",
+            value: "add",
+          },
+          {
+            name: "remove",
+            value: "remove",
           }
         )
     ),
 
   async execute(interaction) {
-    await userschema.findOne({ userID: interaction.options.getUser('user').id })
-      .then(async result => {
+    await userschema
+      .findOne({ userID: interaction.options.getUser("user").id })
+      .then(async (result) => {
         if (!Staff.includes(interaction.user.id)) {
           const errorembed = new MessageEmbed()
             .setColor("RED")
             .setTitle("Wopps")
-            .setDescription(
-              "You are not allowed to use this command"
-            );
+            .setDescription("You are not allowed to use this command");
 
           return interaction.reply({
             embeds: [errorembed],
             ephemeral: true,
           });
         }
-        if (!result || interaction.options.getUser('user').bot) {
+        if (!result || interaction.options.getUser("user").bot) {
           const errorembed = new MessageEmbed()
             .setColor("RED")
             .setTitle("Wopps")
-            .setDescription(
-              "Could not find the user"
-            );
+            .setDescription("Could not find the user");
 
           return interaction.reply({
             embeds: [errorembed],
@@ -59,170 +63,215 @@ module.exports = {
           });
         }
 
-        if (interaction.options.getString('category') == 'add') {
-
+        if (interaction.options.getString("category") == "add") {
           const badgeEmbed = new MessageEmbed()
             .setColor("#5865f4")
             .setTitle("Badge")
-            .setDescription('Please select the badges you want to give the user')
+            .setDescription(
+              "Please select the badges you want to give the user"
+            );
 
-          const row = new MessageActionRow()
-            .addComponents(
-              new MessageSelectMenu()
-                .setCustomId('badge-select')
-                .setPlaceholder('Nothing selected')
-                .addOptions({
+          const row = new MessageActionRow().addComponents(
+            new MessageSelectMenu()
+              .setCustomId("badge-select")
+              .setPlaceholder("Nothing selected")
+              .addOptions(
+                {
                   emoji: {
-                    id: '977994687312969738',
-                    name: 'Staff'
+                    id: "977994687312969738",
+                    name: "Staff",
                   },
-                  label: 'Staff',
-                  description: 'For team members of the bot',
-                  value: 'staff'
+                  label: "Staff",
+                  description: "For team members of the bot",
+                  value: "staff",
                 },
-                  {
-                    emoji: {
-                      id: '977996164458766396',
-                      name: 'Developer'
-                    },
-                    label: 'Developer',
-                    description: 'For team members programming the bot',
-                    value: 'developer'
+                {
+                  emoji: {
+                    id: "977996164458766396",
+                    name: "Developer",
                   },
-                  {
-                    emoji: {
-                      id: '977994824038875166',
-                      name: 'Verified'
-                    },
-                    label: 'Verifed',
-                    description: 'For members who have verified their account',
-                    value: 'verified'
+                  label: "Developer",
+                  description: "For team members programming the bot",
+                  value: "developer",
+                },
+                {
+                  emoji: {
+                    id: "977994824038875166",
+                    name: "Verified",
                   },
-                  {
-                    emoji: {
-                      id: '977994687208116274',
-                      name: 'Partner'
-                    },
-                    label: 'Partner',
-                    description: 'For members who are partners of the bot',
-                    value: 'partner'
+                  label: "Verifed",
+                  description: "For members who have verified their account",
+                  value: "verified",
+                },
+                {
+                  emoji: {
+                    id: "977994687208116274",
+                    name: "Partner",
                   },
-                  {
-                    emoji: {
-                      id: '977994686851579906',
-                      name: 'Featured'
-                    },
-                    label: 'Featured',
-                    description: 'For members who helped the team with building the bot.',
-                    value: 'featured'
-                  })
-                .setMinValues(1),
-            )
+                  label: "Partner",
+                  description: "For members who are partners of the bot",
+                  value: "partner",
+                },
+                {
+                  emoji: {
+                    id: "977994686851579906",
+                    name: "Featured",
+                  },
+                  label: "Featured",
+                  description:
+                    "For members who helped the team with building the bot.",
+                  value: "featured",
+                }
+              )
+              .setMinValues(1)
+          );
 
-          await interaction.reply({ embeds: [badgeEmbed], components: [row], ephemeral: true })
+          await interaction.reply({
+            embeds: [badgeEmbed],
+            components: [row],
+            ephemeral: true,
+          });
 
-          const listener = interaction.channel.createMessageComponentCollector()
-          listener.on('collect', async (selectMenu) => {
-            if (selectMenu.customId == 'badge-select') {
-              const badges = selectMenu.values
-              await badges.forEach(async badge => {
-                if (result.badges.findIndex(b => b == badge) == -1) {
+          const listener =
+            interaction.channel.createMessageComponentCollector();
+          listener.on("collect", async (selectMenu) => {
+            if (selectMenu.customId == "badge-select") {
+              const badges = selectMenu.values;
+              console.log(badges);
+              await badges.forEach(async (badge) => {
+                if (result.badges.findIndex((b) => b == badge) !== -1) {
                   await result.badges.push(badge);
                 } else return;
               });
 
-              await userschema.findOneAndUpdate({ userId: interaction.options.getUser('user').id }, { badges: result.badges }).catch()
+              await userschema
+                .findOneAndUpdate(
+                  { userId: interaction.options.getUser("user").id },
+                  { badges: badges }
+                )
+                .catch();
 
               const badgeEmbed = new MessageEmbed()
                 .setColor("#5865f4")
                 .setTitle("Badges Added")
-                .setDescription('The badges ' + badges.join(', ') + ' have been added to the user')
+                .setDescription(
+                  "The badges " +
+                    badges.join(", ") +
+                    " have been added to the user"
+                );
 
-              return await selectMenu.reply({ embeds: [badgeEmbed], ephemeral: true })
+              return await selectMenu.reply({
+                embeds: [badgeEmbed],
+                ephemeral: true,
+              });
             }
-          })
-
+          });
         } else {
           const badgeEmbed = new MessageEmbed()
             .setColor("#5865f4")
             .setTitle("Badge Modal")
-            .setDescription('Please select the badges you want to remove the user')
+            .setDescription(
+              "Please select the badges you want to remove the user"
+            );
 
-          const row = new MessageActionRow()
-            .addComponents(
-              new MessageSelectMenu()
-                .setCustomId('badge-select')
-                .setPlaceholder('Nothing selected')
-                .addOptions({
+          const row = new MessageActionRow().addComponents(
+            new MessageSelectMenu()
+              .setCustomId("badge-select")
+              .setPlaceholder("Nothing selected")
+              .addOptions(
+                {
                   emoji: {
-                    id: '977994687312969738',
-                    name: 'Staff'
+                    id: "977994687312969738",
+                    name: "Staff",
                   },
-                  label: 'Staff',
-                  description: 'For team members of the bot',
-                  value: 'staff'
+                  label: "Staff",
+                  description: "For team members of the bot",
+                  value: "staff",
                 },
-                  {
-                    emoji: {
-                      id: '977996164458766396',
-                      name: 'Developer'
-                    },
-                    label: 'Developer',
-                    description: 'For team members programming the bot',
-                    value: 'developer'
+                {
+                  emoji: {
+                    id: "977996164458766396",
+                    name: "Developer",
                   },
-                  {
-                    emoji: {
-                      id: '977994824038875166',
-                      name: 'Verified'
-                    },
-                    label: 'Verifed',
-                    description: 'For members who have verified their account',
-                    value: 'verified'
+                  label: "Developer",
+                  description: "For team members programming the bot",
+                  value: "developer",
+                },
+                {
+                  emoji: {
+                    id: "977994824038875166",
+                    name: "Verified",
                   },
-                  {
-                    emoji: {
-                      id: '977994687208116274',
-                      name: 'Partner'
-                    },
-                    label: 'Partner',
-                    description: 'For members who are partners of the bot',
-                    value: 'partner'
+                  label: "Verifed",
+                  description: "For members who have verified their account",
+                  value: "verified",
+                },
+                {
+                  emoji: {
+                    id: "977994687208116274",
+                    name: "Partner",
                   },
-                  {
-                    emoji: {
-                      id: '977994686851579906',
-                      name: 'Featured'
-                    },
-                    label: 'Featured',
-                    description: 'For members who helped the team with building the bot.',
-                    value: 'featured'
-                  })
-                .setMinValues(1),
-            )
+                  label: "Partner",
+                  description: "For members who are partners of the bot",
+                  value: "partner",
+                },
+                {
+                  emoji: {
+                    id: "977994686851579906",
+                    name: "Featured",
+                  },
+                  label: "Featured",
+                  description:
+                    "For members who helped the team with building the bot.",
+                  value: "featured",
+                }
+              )
+              .setMinValues(1)
+          );
 
-          await interaction.reply({ embeds: [badgeEmbed], components: [row], ephemeral: true })
+          await interaction.reply({
+            embeds: [badgeEmbed],
+            components: [row],
+            ephemeral: true,
+          });
 
-          const listener = interaction.channel.createMessageComponentCollector()
-          listener.on('collect', async (selectMenu) => {
-            if (selectMenu.customId == 'badge-select') {
-              const badges = selectMenu.values
-              await badges.forEach(async badge => {
-                if (result.badges.findIndex(b => b == badge) !== -1) {
-                  await result.badges.splice(result.badges.findIndex(b => b == badge), 1);
+          const listener =
+            interaction.channel.createMessageComponentCollector();
+          listener.on("collect", async (selectMenu) => {
+            if (selectMenu.customId == "badge-select") {
+              const badges = selectMenu.values;
+              console.log(badges);
+              await badges.forEach(async (badge) => {
+                if (result.badges.findIndex((b) => b == badge) !== -1) {
+                  await result.badges.splice(
+                    result.badges.findIndex((b) => b == badge),
+                    1
+                  );
                 } else return;
               });
 
-              await userschema.findOneAndUpdate({ userId: interaction.options.getUser('user').id }, { badges: result.badges }).catch()
+              await userschema
+                .findOneAndUpdate(
+                  { userId: interaction.options.getUser("user").id },
+                  { badges: badges }
+                )
+                .catch();
 
               const badgeEmbed = new MessageEmbed()
                 .setColor("#5865f4")
                 .setTitle("Badges Removed")
-                .setDescription('The badges ' + badges.join(', ') + ' have been removed from the user')
+                .setDescription(
+                  "The badges " +
+                    badges.join(", ") +
+                    " have been removed from the user"
+                );
 
-              await selectMenu.reply({ embeds: [badgeEmbed], ephemeral: true })
+              await selectMenu.reply({
+                embeds: [badgeEmbed],
+                ephemeral: true,
+              });
             }
-          })
+          });
         }
       });
   },
