9f183166b329899c13c6dc2868cb2a8a6eeb0a2c
Sigmaxii <xiisigma@gmail.com>
2022-07-06 03:34:22 +0300

    help.js
    Good looking help command with buttons & collectors .
     +++ added 3 links in the config.json file. since its open source it will be easier for people to edit their most shared links

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,85 +1,119 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
-
+const config = require('../../config.json');
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('help')
-    .setDescription("Returns the bot's help embed"),
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription("Returns the bot's help embed")
+        //    .addStringOption(option =>
+        //      option
+        //  .setName('command')
+        //    .setDescription('Select a specific command to get help')
+        ///     .addChoices(
+        ///
+        //             {
+        //               name: 'portfolio',
+        //             value: 'portfolio'
+        //       }, {
+        //         name: 'register',
+        //       value: 'register'
+        // }, {
+        //
+        //      name: 'set',
+        //    value: "set"
+        // },
+        //                {
+        //                  name: 'ping',
+        //                value: 'ping'
+        //          }, {
+        //            name: 'badge',
+        //          value: 'badge'
+        //    }
 
-  async execute(interaction, client) {
-    const embed = new MessageEmbed()
+    // ))
+    ,
 
-      .setColor('#5865f4')
-      .setTitle('Portfolio Bot')
-      .setDescription(
-        'Show off your projects with your own native Discord Portfolio',
-      )
-      .addField('🐟 User Commands', 'Commands only accessible to normal users', false)
-      .addFields(
-        {
-          name: '**/portfolio** [user]',
-          value: 'View your portfolio',
-          inline: true,
+    async execute(interaction, client) {
 
-        },
-        {
-          name: '**/register**',
-          value: 'Register your own custom Portfolio',
-          inline: true,
+        const home = new MessageEmbed()
+            .setColor('#5865f4')
+            .setTitle('Portfolio Bot')
+            .setDescription(`>>> Use the buttons, or \`/help command:\`, to view commands base on their category!\n\ **[Invite Me](${config.invite})  |  [Support Server](${config.support}) | [Source Code](${config.srccode})**
+        `)
 
-        },
-        {
-          name: '**/set** [color/description]',
-          value: 'Change settings about your Portfolio',
-          inline: true,
+        let but1 = new MessageButton().setCustomId('home').setLabel('Home').setStyle('PRIMARY');
+        let but2 = new MessageButton().setCustomId('cmd').setLabel('Commands').setStyle('PRIMARY');
+        let but3 = new MessageButton().setLabel('Invite').setStyle('LINK').setEmoji('🔗').setURL(`${config.invite}`);
+        let but4 = new MessageButton().setLabel('Source Code').setStyle('LINK').setEmoji('978293783278026762').setURL(`${config.srccode}`);
 
-        },
-        {
-          name: '**/ping**',
-          value: "Returns the bot's ping status",
-          inline: true,
-        },
-        {
-          name: '**/help**',
-          value: 'Shows this embed',
-          inline: true,
-        },
-      )
-      .addField('💫 Bot Owner Commands', 'Commands only accessible to the bot owners', false)
-      .addFields(
-        {
-          name: '**/badge** [user] [catergory]',
-          value: 'Add/Remove badges',
-          inline: true,
+        let editEmbed = new MessageEmbed();
+        const m = await interaction.reply({ embeds: [home], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] })
 
-        },
-      );
 
-    const button = new MessageActionRow().addComponents(
-      new MessageButton()
-        .setLabel('Invite')
-        .setStyle('LINK')
-        .setEmoji('🔗')
-        .setURL(
-          `https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=0&scope=bot%20applications.commands`,
-        ),
-      new MessageButton()
-        .setLabel('Source Code')
-        .setStyle('LINK')
-        .setEmoji('978293783278026762')
-        .setURL(
-          'https://github.com/mezotv/portfolio-bot',
-        ),
-    );
+        const collector = interaction.channel.createMessageComponentCollector({
+            filter: (b) => {
+                if (b.user.id === interaction.user.id) return true;
+                else {
+                    b.reply({ ephemeral: true, embeds: [new MessageEmbed().setColor('#ff0000').setDescription(`Only **${interaction.user.tag}** can use this button, if you want then you've to run the command again.`)] });
+                    return false;
+                };
+            },
+            time: 120000,
+            idle: 120000 / 2
+        });
+        collector.on("end", async() => {
+            await interaction.editReply({ components: [new MessageActionRow().addComponents(but1.setDisabled(true), but2.setDisabled(true), but3.setDisabled(true), but4.setDisabled(true))] }).catch(() => {});
+        });
 
-    await interaction.reply({
-      embeds: [embed],
-      components: [button],
-    });
-    setTimeout(() => {
-      button.components[0].setDisabled(true);
-      button.components[1].setDisabled(true);
-      interaction.editReply({ embeds: [embed], components: [button] });
-    }, 120000);
-  },
+        collector.on('collect', async(b) => {
+            if (b.customId === "home") {
+                return await interaction.editReply({ embeds: [home], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] })
+            }
+            if (b.customId === "cmd") {
+                editEmbed.setColor('#5865f4')
+                    .setTitle('Portfolio Bot')
+                    .setDescription(
+                        'Show off your projects with your own native Discord Portfolio',
+                    )
+                    .addField('🐟 User Commands', 'Commands only accessible to normal users', false)
+                    .addFields({
+                        name: '**/portfolio** [user]',
+                        value: 'View your portfolio',
+                        inline: true,
+
+                    }, {
+                        name: '**/register**',
+                        value: 'Register your own custom Portfolio',
+                        inline: true,
+
+                    }, {
+                        name: '**/set** [color/description]',
+                        value: 'Change settings about your Portfolio',
+                        inline: true,
+
+                    }, {
+                        name: '**/ping**',
+                        value: "Returns the bot's ping status",
+                        inline: true,
+                    }, {
+                        name: '**/help**',
+                        value: 'Shows this embed',
+                        inline: true,
+                    }, )
+                    .addField('💫 Bot Owner Commands', 'Commands only accessible to the bot owners', false)
+                    .addFields({
+                        name: '**/badge** [user] [catergory]',
+                        value: 'Add/Remove badges',
+                        inline: true,
+
+                    }, )
+                return await interaction.editReply({ embeds: [editEmbed], components: [new MessageActionRow().addComponents(but1, but2, but3, but4)] })
+            }
+        });
+        return true;
+
+
+
+
+    },
 };
